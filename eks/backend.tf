187a76290f17335270e7854cb99bd3b1a16a5ddd
srinathg369 <81870393+srinathg369@users.noreply.github.com>
2024-08-28 17:36:39 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "sri-ews-baket1"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
